f7356f3d4439f8f0d773639843ed40ed575a3d79
LeafmanZ <zielemanj@gmail.com>
2024-07-25 13:02:48 -0400

    added more progress metrics like bytes moved

--- s3_transfer.py
+++ s3_transfer.py
@@ -53,7 +53,7 @@ dt_data_dict = {
     "dst_endpoint_urls": dst_endpoint_urls,
     "max_workers": max_workers,
     "epoch_time_start": int(start_time),
-    "completed": False,
+    "completed": "Running",
     "objects_moved": [],
     "epoch_time_end": False,
     "total_time_seconds": False,
@@ -73,108 +73,112 @@ print(f"Configuration details saved to {dt_data_json_dir}")
 ###
 # END: SAVE CONFIGURATION INFORMATION TO JSON 
 ###
+try:
+    # create our source and destination s3 clients so we can interact with our buckets
+    src_s3_client = create_s3_client(src_access_key, src_secret_access_key, src_region, src_endpoint_urls[0])
+    dst_s3_client = create_s3_client(dst_access_key, dst_secret_access_key, dst_region, dst_endpoint_urls[0])
 
-# create our source and destination s3 clients so we can interact with our buckets
-src_s3_client = create_s3_client(src_access_key, src_secret_access_key, src_region, src_endpoint_urls[0])
-dst_s3_client = create_s3_client(dst_access_key, dst_secret_access_key, dst_region, dst_endpoint_urls[0])
+    # Get the objects in our destination and source buckets to compare missing objects
+    src_objects = list_objects(src_bucket, src_prefix, src_s3_client, isSnow=(src_region=='snow'))
+    dst_objects = list_objects(dst_bucket, dst_prefix, dst_s3_client, isSnow=(dst_region=='snow'))
 
-# Get the objects in our destination and source buckets to compare missing objects
-src_objects = list_objects(src_bucket, src_prefix, src_s3_client, isSnow=(src_region=='snow'))
-dst_objects = list_objects(dst_bucket, dst_prefix, dst_s3_client, isSnow=(dst_region=='snow'))
+    # Dictionary of objects that have been successfully moved and are identical in both source and destination
+    objects_synced = {key: src_objects[key] for key in src_objects if (key in dst_objects and src_objects[key] == dst_objects[key])}
+    # Dictionary of objects that have not been moved or differ from the source to destination
+    objects_not_synced = {key: src_objects[key] for key in src_objects if (key not in dst_objects or src_objects[key] != dst_objects[key])}
 
-# Dictionary of objects that have been successfully moved and are identical in both source and destination
-objects_synced = {key: src_objects[key] for key in src_objects if (key in dst_objects and src_objects[key] == dst_objects[key])}
-# Dictionary of objects that have not been moved or differ from the source to destination
-objects_not_synced = {key: src_objects[key] for key in src_objects if (key not in dst_objects or src_objects[key] != dst_objects[key])}
+    ###
+    # BEGIN: UPDATE BUCKET OBJECT INFORMATION
+    ###
+    update_dt_data_dict = {"start_num_objects_synced": len(objects_synced),
+                        "start_num_objects_not_synced": len(objects_not_synced),
+                        "final_num_objects_synced": len(objects_synced),
+                        "final_num_objects_not_synced": len(objects_not_synced),
+                        "percent_completed": len(objects_synced)/(len(objects_synced)+len(objects_not_synced)),
+                        "total_bytes_to_move": sum(objects_not_synced.values())}
+    update_json(dt_data_json_dir, update_dt_data_dict)
+    ###
+    # END: UPDATE BUCKET OBJECT INFORMATION
+    ###
 
-###
-# BEGIN: UPDATE BUCKET OBJECT INFORMATION
-###
-update_dt_data_dict = {"start_num_objects_synced": len(objects_synced),
-                    "start_num_objects_not_synced": len(objects_not_synced),
-                    "final_num_objects_synced": len(objects_synced),
-                    "final_num_objects_not_synced": len(objects_not_synced),
-                    "percent_completed": len(objects_synced)/(len(objects_synced)+len(objects_not_synced)),
-                    "total_bytes_to_move": sum(objects_not_synced.values())}
-update_json(dt_data_json_dir, update_dt_data_dict)
-###
-# END: UPDATE BUCKET OBJECT INFORMATION
-###
+    # Distribute endpoint_urls evenly over the amount of objects we need to move
+    endpoint_url_distribution = []
+    for i in range(len(objects_not_synced)):
+        inbound = src_endpoint_urls[i % len(src_endpoint_urls)]
+        outbound = dst_endpoint_urls[i % len(dst_endpoint_urls)]
+        endpoint_url_distribution.append((inbound, outbound))
 
-# Distribute endpoint_urls evenly over the amount of objects we need to move
-endpoint_url_distribution = []
-for i in range(len(objects_not_synced)):
-    inbound = src_endpoint_urls[i % len(src_endpoint_urls)]
-    outbound = dst_endpoint_urls[i % len(dst_endpoint_urls)]
-    endpoint_url_distribution.append((inbound, outbound))
+    # Function to run the sync_s3_obj.py script, this is necessary to avoid GIL bottleneck
+    def sync_s3_obj(src_bucket, dst_bucket, src_key, dst_key, bytes, src_endpoint_url, dst_endpoint_url, dt_data_json_dir, benchmark_progress):
+        command = f"python sync_s3_obj.py {src_bucket} {dst_bucket} {src_key} {dst_key} {bytes} {src_endpoint_url} {dst_endpoint_url} {dt_data_json_dir}"
+        subprocess.run(command, shell=True, check=True)
+        if benchmark_progress:
+            # Get the objects in our destination buckets to compare missing objects
+            dst_objects = list_objects(dst_bucket, dst_prefix, dst_s3_client, isSnow=(dst_region=='snow'))
 
-# Function to run the sync_s3_obj.py script, this is necessary to avoid GIL bottleneck
-def sync_s3_obj(src_bucket, dst_bucket, src_key, dst_key, bytes, src_endpoint_url, dst_endpoint_url, dt_data_json_dir, benchmark_progress):
-    command = f"python sync_s3_obj.py {src_bucket} {dst_bucket} {src_key} {dst_key} {bytes} {src_endpoint_url} {dst_endpoint_url} {dt_data_json_dir}"
-    subprocess.run(command, shell=True, check=True)
-    if benchmark_progress:
-        # Get the objects in our destination buckets to compare missing objects
-        dst_objects = list_objects(dst_bucket, dst_prefix, dst_s3_client, isSnow=(dst_region=='snow'))
+            # Dictionary of objects that have been successfully moved and are identical in both source and destination
+            objects_synced = {key: src_objects[key] for key in src_objects if (key in dst_objects and src_objects[key] == dst_objects[key])}
+            # Dictionary of objects that have not been moved or differ from the source to destination
+            objects_not_synced = {key: src_objects[key] for key in src_objects if (key not in dst_objects or src_objects[key] != dst_objects[key])}
 
-        # Dictionary of objects that have been successfully moved and are identical in both source and destination
-        objects_synced = {key: src_objects[key] for key in src_objects if (key in dst_objects and src_objects[key] == dst_objects[key])}
-        # Dictionary of objects that have not been moved or differ from the source to destination
-        objects_not_synced = {key: src_objects[key] for key in src_objects if (key not in dst_objects or src_objects[key] != dst_objects[key])}
+            ###
+            # BEGIN: UPDATE BUCKET OBJECT INFORMATION
+            ###
+            update_dt_data_dict = {"final_num_objects_synced": len(objects_synced),
+                                "final_num_objects_not_synced": len(objects_not_synced),
+                                "percent_completed": len(objects_synced)/(len(objects_synced)+len(objects_not_synced)),
+                                "bytes_moved": sum(objects_synced.values()),
+                                "bytes_left_to_move": sum(objects_not_synced.values()),}
+            update_json(dt_data_json_dir, update_dt_data_dict)
+            ###
+            # END: UPDATE BUCKET OBJECT INFORMATION
+            ###
 
-        ###
-        # BEGIN: UPDATE BUCKET OBJECT INFORMATION
-        ###
-        update_dt_data_dict = {"final_num_objects_synced": len(objects_synced),
-                            "final_num_objects_not_synced": len(objects_not_synced),
-                            "percent_completed": len(objects_synced)/(len(objects_synced)+len(objects_not_synced)),
-                            "bytes_moved": sum(objects_synced.values()),
-                            "bytes_left_to_move": sum(objects_not_synced.values()),}
-        update_json(dt_data_json_dir, update_dt_data_dict)
-        ###
-        # END: UPDATE BUCKET OBJECT INFORMATION
-        ###
+    # Spawn individual sync_s3_obj processes moving 1 object per process, in parallel up to the amount of max_workers at a time.
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for i, obj_key in enumerate(objects_not_synced.keys()):
+            src_key = f"{src_prefix.rstrip('/')}/{obj_key}".lstrip('/')
+            dst_key = f"{dst_prefix.rstrip('/')}/{obj_key}".lstrip('/')
+            src_endpoint_url, dst_endpoint_url = endpoint_url_distribution[i]
+            if i % max_workers == 0: # Every nth object sync we spawn, update our sync status every update_benchmark_interval 
+                benchmark_progress = True
+            else:
+                benchmark_progress = False
+            futures.append(executor.submit(sync_s3_obj, src_bucket, dst_bucket, src_key, dst_key, objects_not_synced[obj_key],src_endpoint_url, dst_endpoint_url, dt_data_json_dir, benchmark_progress))
 
-# Spawn individual sync_s3_obj processes moving 1 object per process, in parallel up to the amount of max_workers at a time.
-with ThreadPoolExecutor(max_workers=max_workers) as executor:
-    futures = []
-    for i, obj_key in enumerate(objects_not_synced.keys()):
-        src_key = f"{src_prefix.rstrip('/')}/{obj_key}".lstrip('/')
-        dst_key = f"{dst_prefix.rstrip('/')}/{obj_key}".lstrip('/')
-        src_endpoint_url, dst_endpoint_url = endpoint_url_distribution[i]
-        if i % max_workers == 0: # Every nth object sync we spawn, update our sync status every update_benchmark_interval 
-            benchmark_progress = True
-        else:
-            benchmark_progress = False
-        futures.append(executor.submit(sync_s3_obj, src_bucket, dst_bucket, src_key, dst_key, objects_not_synced[obj_key],src_endpoint_url, dst_endpoint_url, dt_data_json_dir, benchmark_progress))
+        # Wait for all futures to complete
+        wait(futures)
 
-    # Wait for all futures to complete
-    wait(futures)
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Total time taken: {total_time:.2f} seconds")
 
-end_time = time.time()
-total_time = end_time - start_time
-print(f"Total time taken: {total_time:.2f} seconds")
+    ###
+    # BEGIN: SAVE FINISHING COMPLETION INFORMATION TO JSON
+    ###
+    # Get the objects in our destination buckets to compare missing objects
+    dst_objects = list_objects(dst_bucket, dst_prefix, dst_s3_client, isSnow=(dst_region=='snow'))
 
-###
-# BEGIN: SAVE FINISHING COMPLETION INFORMATION TO JSON
-###
-# Get the objects in our destination buckets to compare missing objects
-dst_objects = list_objects(dst_bucket, dst_prefix, dst_s3_client, isSnow=(dst_region=='snow'))
+    # Dictionary of objects that have been successfully moved and are identical in both source and destination
+    objects_synced = {key: src_objects[key] for key in src_objects if (key in dst_objects and src_objects[key] == dst_objects[key])}
+    # Dictionary of objects that have not been moved or differ from the source to destination
+    objects_not_synced = {key: src_objects[key] for key in src_objects if (key not in dst_objects or src_objects[key] != dst_objects[key])}
 
-# Dictionary of objects that have been successfully moved and are identical in both source and destination
-objects_synced = {key: src_objects[key] for key in src_objects if (key in dst_objects and src_objects[key] == dst_objects[key])}
-# Dictionary of objects that have not been moved or differ from the source to destination
-objects_not_synced = {key: src_objects[key] for key in src_objects if (key not in dst_objects or src_objects[key] != dst_objects[key])}
-
-new_dt_data_dict = {'final_num_objects_synced': len(objects_synced),
-                    'final_num_objects_not_synced': len(objects_not_synced),
-                    "percent_completed": len(objects_synced)/(len(objects_synced)+len(objects_not_synced)),
-                    "bytes_moved": sum(objects_synced.values()),
-                    "bytes_left_to_move": sum(objects_not_synced.values()),
-                    'objects_failed_to_move': objects_not_synced,
-                    'epoch_time_end': int(end_time),
-                    'total_time_seconds': total_time,
-                    'completed': True}
-update_json(dt_data_json_dir, new_dt_data_dict)
+    new_dt_data_dict = {'final_num_objects_synced': len(objects_synced),
+                        'final_num_objects_not_synced': len(objects_not_synced),
+                        "percent_completed": len(objects_synced)/(len(objects_synced)+len(objects_not_synced)),
+                        "bytes_moved": sum(objects_synced.values()),
+                        "bytes_left_to_move": sum(objects_not_synced.values()),
+                        'objects_failed_to_move': objects_not_synced,
+                        'epoch_time_end': int(end_time),
+                        'total_time_seconds': total_time,
+                        'completed': 'Completed'}
+    update_json(dt_data_json_dir, new_dt_data_dict)
+except:
+    new_dt_data_dict = {
+        'completed': 'Failed'
+    }
 ###
 # END: SAVE FINISHING COMPLETION INFORMATION TO JSON
 ###
