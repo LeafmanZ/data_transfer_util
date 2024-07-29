3b5f2c5173930d3cff78f2f41d7ea635a74b0b6a
LeafmanZ <zielemanj@gmail.com>
2024-07-29 15:20:51 -0400

    api readme and example update

--- api_test_example.py
+++ api_test_example.py
@@ -1,27 +1,27 @@
 import requests
 import json
 
-API_URL = 'http://127.0.0.1:5000'
+API_URL = 'http://127.0.0.1:443'
 API_TOKEN = 'your_secure_token_here'
 HEADERS = {'Authorization': API_TOKEN}
 
 def test_list_logs(start_time, end_time):
     print("Testing /logs endpoint...")
-    response = requests.get(f"{API_URL}/logs", headers=HEADERS, params={'start': start_time, 'end': end_time})
+    response = requests.get(f"{API_URL}/logs", headers=HEADERS, params={'start': start_time, 'end': end_time}, verify=False)
     print(f"Status Code: {response.status_code}")
     print("Response:")
     print(json.dumps(response.json(), indent=4))
 
 def test_get_log(epoch_time, retrieve_objects_moved=False):
     print(f"Testing /log/{epoch_time} endpoint...")
-    response = requests.get(f"{API_URL}/log/{epoch_time}", headers=HEADERS, params={'retrieve_objects_moved': retrieve_objects_moved})
+    response = requests.get(f"{API_URL}/log/{epoch_time}", headers=HEADERS, params={'retrieve_objects_moved': retrieve_objects_moved}, verify=False)
     print(f"Status Code: {response.status_code}")
     print("Response:")
     print(json.dumps(response.json(), indent=4))
 
 def test_get_latest_log(retrieve_objects_moved=False):
     print("Testing /log/latest endpoint...")
-    response = requests.get(f"{API_URL}/log/latest", headers=HEADERS, params={'retrieve_objects_moved': retrieve_objects_moved})
+    response = requests.get(f"{API_URL}/log/latest", headers=HEADERS, params={'retrieve_objects_moved': retrieve_objects_moved}, verify=False)
     print(f"Status Code: {response.status_code}")
     print("Response:")
     print(json.dumps(response.json(), indent=4))
@@ -29,8 +29,8 @@ def test_get_latest_log(retrieve_objects_moved=False):
 if __name__ == '__main__':
     # Adjust these parameters according to your test data
     start_time = 1721835440  # Example start time epoch
-    end_time = 1721835460    # Example end time epoch
-    epoch_time = 1721835441  # Example epoch time
+    end_time = 1821835460    # Example end time epoch
+    epoch_time = 1722011110  # Example epoch time
 
     line_separator = '\n##########################\n'
     print(line_separator)
